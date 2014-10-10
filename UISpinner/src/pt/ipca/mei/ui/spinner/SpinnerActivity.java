23979e409ced438e5983a4d7a1073bc39a6bb695
Diogo Ferreira <diogo.cferreiraa@gmail.com>
2014-10-10 20:24:06 +0100

    teste
    sdsd

---
+++ UISpinner/src/pt/ipca/mei/ui/spinner/SpinnerActivity.java
@@ -0,0 +1,41 @@
+package pt.ipca.mei.ui.spinner;
+
+import course.examples.UI.Spinner.R;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class SpinnerActivity extends Activity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.main);
+
+		Spinner spinner = (Spinner) findViewById(R.id.spinner);
+		
+		ArrayAdapter<CharSequence> adapter = 
+				ArrayAdapter.createFromResource(
+				this, R.array.places, R.layout.dropdown_item);
+	
+		spinner.setAdapter(adapter);
+		
+		spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+			public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+				Toast.makeText(
+						parent.getContext(),
+						"Vais passar férias em: "
+								+ parent.getItemAtPosition(pos).toString(),
+						Toast.LENGTH_LONG).show();
+			}
+
+			public void onNothingSelected(AdapterView<?> parent) {
+			}
+		});
+	}
+}
